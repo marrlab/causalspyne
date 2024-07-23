f6ee0f70930e127549416904aaa8d4575a45262b
smilesun <smilesun.east@gmail.com>
2024-07-23 16:33:38 +0200

    put py into folder

--- causalSpyne/dag_gen.py
+++ causalSpyne/dag_gen.py
@@ -1,8 +1,11 @@
+"""
+concrete class to generate simple DAGs
+"""
 import numpy as np
 
 
 class Erdos_Renyi():
-    def __call__(self, num_nodes, degree, weight_range):
+    def __call__(self, num_nodes, degree, list_weight_range):
         prob = float(degree) / (num_nodes - 1)
         # lower triagular, k=-1 is the lower off diagonal
         mat_b = np.tril((
@@ -10,7 +13,7 @@ class Erdos_Renyi():
             k=-1)
         mat_perm = np.random.permutation(np.eye(num_nodes, num_nodes))  # permutes first axis only
         mat_b_permuted = mat_perm.T.dot(mat_b).dot(mat_perm)
-        mat_weight = np.random.uniform(low=weight_range[0], high=weight_range[1],
+        mat_weight = np.random.uniform(low=list_weight_range[0], high=list_weight_range[1],
                                        size=[num_nodes, num_nodes])
         mat_weight[np.random.rand(num_nodes, num_nodes) < 0.5] *= -1
 
@@ -19,18 +22,18 @@ class Erdos_Renyi():
 
 
 class GenDAGER():
-    def __init__(self, num_nodes, degree, weight_range):
+    def __init__(self, num_nodes, degree, list_weight_range):
         self.num_nodes = num_nodes
         self.degree = degree
-        self.weight_range = weight_range
+        self.list_weight_range = list_weight_range
         self.gen = Erdos_Renyi()
 
     def genDAG(self, num_nodes=None):
         if num_nodes is None:
             num_nodes = self.num_nodes
-        matrix, _ = self.gen(num_nodes, self.degree, self.weight_range)
+        matrix, _ = self.gen(num_nodes, self.degree, self.list_weight_range)
         return matrix
 
 
 def test_erdos_renyi():
-    Erdos_Renyi()(num_nodes=3, degree=2, weight_range=[3, 5])
+    Erdos_Renyi()(num_nodes=3, degree=2, list_weight_range=[3, 5])
--- tests/test_recursive_gen.py
+++ tests/test_recursive_gen.py
@@ -5,7 +5,7 @@ from dag_gen import Erdos_Renyi, GenDAGER
 
 def test_recursive_gen():
 
-    dag_gen = GenDAGER(num_nodes=3, degree=2, weight_range=[3, 5])
+    dag_gen = GenDAGER(num_nodes=3, degree=2, list_weight_range=[3, 5])
     getter_num_nodes = NumNodesPerCluster()
     gen = RecursiveGraphGen(dag_generator=dag_gen,
                       backbone_density=0.7,
