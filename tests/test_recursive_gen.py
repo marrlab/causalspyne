f35a75a131d5d71f356d4090dd7c4a8502805fdb
smilesun <smilesun.east@gmail.com>
2024-07-24 10:36:38 +0200

    .

--- tests/test_recursive_gen.py
+++ tests/test_recursive_gen.py
@@ -1,6 +1,10 @@
-from causalSpyne.recursive_graph_gen import \
+"""
+test 2 level DAG generation
+"""
+
+from causalSpyne.gen_dag_2level import \
     GenDAG2Level, NumNodesPerCluster
-from causalSpyne.dag_gen import Erdos_Renyi, GenDAG
+from causalSpyne.dag_gen import GenDAG
 
 
 def test_recursive_gen():
@@ -8,6 +12,6 @@ def test_recursive_gen():
     dag_gen = GenDAG(num_nodes=3, degree=2, list_weight_range=[3, 5])
     getter_num_nodes = NumNodesPerCluster()
     gen = GenDAG2Level(dag_generator=dag_gen,
-                            strategy_num_nodes_per_cluster=getter_num_nodes,
-                            num_macro_nodes=3)
+                       strategy_num_nodes_per_cluster=getter_num_nodes,
+                       num_macro_nodes=3)
     gen.run()
