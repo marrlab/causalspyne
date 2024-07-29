70f3aec0230d66c725f2e27ac833298a46e61cc0
smilesun <smilesun.east@gmail.com>
2024-07-29 15:46:11 +0200

    .

--- examples/benchpress/sh_benchpress.sh
+++ examples/benchpress/sh_benchpress.sh
@@ -15,6 +15,7 @@ touch benchpress/resources/constraints/None-bnlearn
 
 cd benchpress
 git checkout dev
+git checkout b2c482fa7089980eaa8769b55b2c2d1dad9c2dc1
 
 snakemake --cores all --use-singularity --configfile config/causalSpyne.json
 
