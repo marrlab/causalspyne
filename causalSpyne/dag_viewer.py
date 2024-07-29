ee070b1f05ea26c099d184c14c252cf291fd05eb
smilesun <smilesun.east@gmail.com>
2024-07-29 17:04:29 +0200

    .

--- causalSpyne/dag_viewer.py
+++ causalSpyne/dag_viewer.py
@@ -6,6 +6,9 @@ create different views for the same DAG by hiding some variables
 class DAGView():
     def __init__(self, dag):
         self._dag = dag
+        self.data_gen = DataGenLinearGaussian(self._dag)
+        self.data_gen.run()
+
 
     def hide(self, toplogical_order_ind_node):
         global_ind = self._dag.list_ind_nodes_sorted[toplogical_order_ind_node]
