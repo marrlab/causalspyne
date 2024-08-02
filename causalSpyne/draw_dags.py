6a1ad1b559ccd31c7128142d78a038d6276978b8
Xudong Sun <smilesun@users.noreply.github.com>
2024-08-02 18:56:07 +0000

    close plot

--- causalSpyne/draw_dags.py
+++ causalSpyne/draw_dags.py
@@ -4,6 +4,7 @@ from networkx.drawing.nx_agraph import graphviz_layout
 
 
 def draw_dags_nx(adj_matrix, dict_ind2name=None, title="dag", show=False):
+    plt.close('all')
     nx_graph = nx.from_numpy_array(adj_matrix, create_using=nx.DiGraph)
     if dict_ind2name:
         nx_graph = nx.relabel_nodes(nx_graph, dict_ind2name)
