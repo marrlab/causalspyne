fb87d28f986f97d5290cee986bbec63ed4604cd1
Xudong Sun <smilesun@users.noreply.github.com>
2024-08-05 16:44:26 +0000

    add test to main

--- causalSpyne/main.py
+++ causalSpyne/main.py
@@ -1,32 +1,28 @@
-import pandas as pd
-from causalSpyne.data_gen import DataGen
 from causalSpyne.gen_dag_2level import GenDAG2Level
 from causalSpyne.dag_gen import GenDAG
 from causalSpyne.dag_viewer import DAGView
 
 
-def gen_partially_observed_data(backbone_num_nodes, backbone_degree,
-                                backbone_list_weight_range,
-                                num_macro_nodes,
-                                num_samples,
-                                output_file_names="output.csv"):
+def gen_partially_observed(list_weight_range=[3,5],
+                           degree=2,
+                           list_confounder2hide=[0.5, 0.9],
+                           size_micro_node_dag=4,
+                           num_macro_nodes=4,
+                           num_sample=200):
     """
-    test linear gaussian data gen
+    sole function as user interface
     """
 
 
-    simple_dag_gen = GenDAG(num_nodes=4, degree=2, list_weight_range=[3, 5])
+    simple_dag_gen = GenDAG(num_nodes=size_micro_node_dag, degree=degree, list_weight_range=list_weight_range)
 
-    dag_gen = GenDAG2Level(dag_generator=simple_dag_gen, num_macro_nodes=4)
-
+    # num_macro_nodes will overwrite behavior
+    dag_gen = GenDAG2Level(dag_generator=simple_dag_gen, num_macro_nodes=num_macro_nodes)
     dag = dag_gen.run()
-    dag.visualize(title="dag_complete_confounder")
+    dag.visualize(title="dag_complete")
     dag.to_binary_csv()
-    gen_data = DataGen(dag)
-    gen_data.gen(num_samples=200)
-    gen_data.to_csv()
 
     subview = DAGView(dag=dag)
-    subview.run(num_samples=200, confound=True, list_nodes2hide=[0.99])
+    subview.run(num_samples=num_sample, confound=True, list_nodes2hide=list_confounder2hide)
     subview.to_csv()
-    subview.visualize(title="dag_marginal_confounder")
+    subview.visualize(title="dag_marginal")
