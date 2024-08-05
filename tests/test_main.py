b9711237fc7a50f420cc99dc032f41beddb00e1f
Xudong Sun <smilesun@users.noreply.github.com>
2024-08-05 20:14:04 +0200

    Create test_main.py (#7)

---
+++ tests/test_main.py
@@ -0,0 +1,9 @@
+from causalSpyne import gen_partially_observed
+
+
+def test_main():
+  gen_partially_observed(degree=2,
+                           list_confounder2hide=[0.5, 0.9],
+                           size_micro_node_dag=4,
+                           num_macro_nodes=4,
+                           num_sample=200)
