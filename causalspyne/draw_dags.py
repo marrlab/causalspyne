8f58ad160bcc874a165d3b2cf2ba80215cf61fc5
Xudong Sun <smilesun@users.noreply.github.com>
2024-08-06 18:07:18 +0200

    lower numpy version, graphviz (#10)
    * .
    
    * use spring layerout instead of graphviz
    
    * .

--- causalspyne/draw_dags.py
+++ causalspyne/draw_dags.py
@@ -6,7 +6,7 @@ import matplotlib.pyplot as plt
 from networkx.drawing.nx_agraph import graphviz_layout
 
 
-def draw_dags_nx(adj_matrix, dict_ind2name=None, title="dag", show=False):
+def draw_dags_nx(adj_matrix, dict_ind2name=None, title="dag", show=False, graphviz=False):
     """
     networkx adjacency matrix (i,j) entry refers to edge from j pointing to i,
     which is opposite to the CausalSpyne convention
@@ -16,7 +16,10 @@ def draw_dags_nx(adj_matrix, dict_ind2name=None, title="dag", show=False):
         adj_matrix.transpose(), create_using=nx.DiGraph)
     if dict_ind2name:
         nx_graph = nx.relabel_nodes(nx_graph, dict_ind2name)
-    pos = graphviz_layout(nx_graph,  prog='dot')
+    if graphviz:
+        pos = graphviz_layout(nx_graph,  prog='dot')
+    else:
+        pos = nx.spring_layout(nx_graph, k=0.5, scale=2)
     nx.draw(nx_graph, pos=pos,
             arrows=True, with_labels=True)
     plt.title(title)
