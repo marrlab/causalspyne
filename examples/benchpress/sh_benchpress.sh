8941cdc3d4faedb3a7e9cf36baea52c9f46e4088
smilesun <smilesun.east@gmail.com>
2024-08-07 10:05:43 +0200

    .

--- examples/benchpress/sh_benchpress.sh
+++ examples/benchpress/sh_benchpress.sh
@@ -1,22 +1,27 @@
-cp output.csv benchpress/resources/data/mydatasets/
 
-cp examples/benchpress/causalspyne.json benchpress/config/causalspyne.json
+cp data_subdag.csv submodules/benchpress/resources/data/mydatasets/
 
-cp examples/benchpress/causalspyne_dev.json benchpress/config/causalspyne.json
+cp submodules/benchpress/config/paper_sachs.json examples/benchpress/causalspyne_master.json
 
-
-cp examples/benchpress/full_dev.json benchpress/config/causalspyne.json
+cp examples/benchpress/causalspyne_master.json submodules/benchpress/config/causalspyne.json
 
 
-cp adj.csv benchpress/resources/adjmat/myadjmats/adj.csv
+#cp examples/benchpress/causalspyne.json submodules/benchpress/config/causalspyne.json
+
+#cp examples/benchpress/causalspyne_dev.json submodules/benchpress/config/causalspyne.json
+
+#cp examples/benchpress/full_dev.json submodules/benchpress/config/causalspyne.json
+
+
+cp adj.csv submodules/benchpress/resources/adjmat/myadjmats/adj.csv
 
 # touch benchpress/resources/constraints/None-pcalg
-echo "{}" > benchpress/resources/constraints/None-pcalg
+# echo "{}" > submodules/benchpress/resources/constraints/None-pcalg
 # touch benchpress/resources/constraints/None-bnlearn
 
-cd benchpress
-git checkout dev
-git checkout b2c482fa7089980eaa8769b55b2c2d1dad9c2dc1
+cd submodules/benchpress
+# git checkout dev
+# git checkout b2c482fa7089980eaa8769b55b2c2d1dad9c2dc1
 
 snakemake --cores all --use-singularity --configfile config/causalspyne.json
 
