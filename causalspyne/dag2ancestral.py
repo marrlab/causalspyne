0915ac91b4a8ace865a174253bb2ef7346c1c319
smilesun <smilesun.east@gmail.com>
2024-10-29 10:50:00 +0100

    improve code quality dag2ancestral

--- causalspyne/dag2ancestral.py
+++ causalspyne/dag2ancestral.py
@@ -14,6 +14,14 @@ def pairwise_combinations(lst):
     return itertools.combinations(lst, 2)
 
 
+def to_binary(matrix):
+    """
+    convert a matrix to 0,1 matrix
+    """
+    binary_matrix = (matrix != 0).astype(int)
+    return binary_matrix
+
+
 class DAG2Ancestral:
     """
     turn DAG into ancestral given list of variabels to hide
@@ -31,34 +39,43 @@ class DAG2Ancestral:
         self.bmat_ancestor = np.transpose(mat)
 
     def run(self, list_hidden):
+        """
+        convert DAG to ancestral
+        """
         self.pre_cal_n_hop()
-        for h in list_hidden:
-            self.deal_children(h)
-            self.deal_parent(h)
+        for hidden in list_hidden:
+            self.deal_children(hidden)
+            self.deal_parent(hidden)
+
         # delete first axis
         temp_mat_row = np.delete(self.mat4ancestral, list_hidden, axis=0)
-
         # delete second axis
         mat_adj_subgraph = np.delete(temp_mat_row, list_hidden, axis=1)
         self.mat4ancestral = mat_adj_subgraph
-        return self.mat4ancestral
+        return to_binary(self.mat4ancestral)
 
-    def is_ancestor(self, c1, c2):
-        flag = self.bmat_ancestor[c2, c1]
+    def is_ancestor(self, global_ind_node_1, global_ind_node_2):
+        """
+        check if the first argument is an ancestor of the second
+        """
+        flag = self.bmat_ancestor[global_ind_node_2, global_ind_node_1]
         return flag
 
-    def deal_parent(self, h):
-        list_parents = self.get_list_parents(h)
-        list_children = self.get_list_children(h)
+    def deal_parent(self, hidden):
+        """
+        connect parent of hidden and child of hidden
+        """
+        list_parents = self.get_list_parents(hidden)
+        list_children = self.get_list_children(hidden)
         for global_ind_parent in list_parents:
             for global_ind_child in list_children:
                 self.mat4ancestral[global_ind_child, global_ind_parent] = 1
 
-    def deal_children(self, h):
+    def deal_children(self, hidden):
         """
-        for d_1, d_2 in children(h) and d_1, d_2 not connected
+        for d_1, d_2 in children(hidden) and d_1, d_2 not connected
         """
-        list_children = self.get_list_children(h)
+        list_children = self.get_list_children(hidden)
         if len(list_children) < 2:
             return
         for pair in pairwise_combinations(list_children):
@@ -72,11 +89,11 @@ class DAG2Ancestral:
                 self.mat4ancestral[c1_global_ind, c2_global_ind] = 1
                 self.mat4ancestral[c2_global_ind, c1_global_ind] = 1
 
-    def get_list_children(self, h):
+    def get_list_children(self, hidden):
         """
         adj[i,j] indicate arrow from j to i
         """
-        column_index = h
+        column_index = hidden
         arr = self.old_adj
         nonzero_indices = np.nonzero(arr[:, column_index])[0]
         # np.nonzero() returns a tuple of arrays.
@@ -84,18 +101,20 @@ class DAG2Ancestral:
         # the input array and contains the indices of non-zero elements
         # along that dimension.
         # nonzero_elements = arr[nonzero_indices, column_index]
-        return list(nonzero_indices)
+        list_non_zero_indices = nonzero_indices.tolist()
+        return list_non_zero_indices
 
-    def get_list_parents(self, h):
+    def get_list_parents(self, hidden):
         """
         adj[i,j] indicate arrow from j to i
         """
-        column_index = h
+        row_index = hidden
         arr = self.old_adj
-        nonzero_indices = np.nonzero(arr[column_index, :])[0]
+        nonzero_indices = np.nonzero(arr[row_index, :])[0]
         # np.nonzero() returns a tuple of arrays.
         # Each array in this tuple corresponds to a dimension of
         # the input array and contains the indices of non-zero elements
         # along that dimension.
         # nonzero_elements = arr[nonzero_indices, column_index]
-        return list(nonzero_indices)
+        list_non_zero_indices = nonzero_indices.tolist()
+        return list_non_zero_indices
