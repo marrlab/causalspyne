5b7d57dc2c63ed9361090ea6f16118fcd0feab2e
smilesun <smilesun.east@gmail.com>
2024-10-29 17:38:06 +0100

    refactor confounder insertion

--- causalspyne/dag_manipulator.py
+++ causalspyne/dag_manipulator.py
@@ -5,57 +5,83 @@ import numpy as np
 
 
 class DAGManipulator:
+    """
+    insert edges to have more confounders
+    """
     def __init__(self, dag, obj_gen_weight, rng):
         self.dag = dag
         self._obj_gen_weight = obj_gen_weight
         self.rng = rng
 
-    def mk_confound(self, ind_arbitrary_confound_input=None, force=True):
+    def mk_confound(self, ind_arbitrary_confound_input=None,
+                    continue_top_up=True,
+                    skip_sink=False):
         """
-        make the current vertex confounder
-        ind_arbitrary_confound_input is the arbitrary index for a node
+        ind_arbitrary_confound_input is the arbitrary index for a node, if
+        does not set as argument, a random index will be generated, the
+        function make the current vertex confounder
+        continue_top_up: if not successful, move up toplogically to try again
+        skip_sink: if a node is sink node (can be a high toplogical order),
+        then does not add edge to other nodes with lower toplogical order
         """
         ind_arbitrary_confound = ind_arbitrary_confound_input
         if ind_arbitrary_confound is None:
-            ind_arbitrary_confound = self.rng.choice(self.dag.list_ind_nodes_sorted)
-            # list_ind_nodes_sorted looks like [8, 3, 10, 9, 100], each entry is an
-            # arbitrary index/name for the node
+            ind_arbitrary_confound = \
+                self.rng.choice(self.dag.list_ind_nodes_sorted)
+            # list_ind_nodes_sorted looks like [8, 3, 10, 9, 100],
+            # each entry is an arbitrary index/name for the node
         # rembmer (i,j) entry means there is arrow from j to i
         # count how many children this current node has
-        nnzero = np.count_nonzero(self.dag.mat_adjacency[:, ind_arbitrary_confound])
+        nnzero = np.count_nonzero(
+            self.dag.mat_adjacency[:, ind_arbitrary_confound])
+        flag_success = False
         if nnzero == 0:  # 0 means sink node
             # sink node can also be quite high in toplogical rank
-            return False
-        if nnzero == 1:  # not a sink node but only parent a single child
+            if not skip_sink:
+                flag_success = self.add_new_edge(ind_arbitrary_confound)
+        elif nnzero == 1:  # not a sink node but only parent a single child
             # add another child to this current node
-            # get toplogical order of this node
-            pos = self.dag.list_ind_nodes_sorted.index(ind_arbitrary_confound)
-            # randomly choose one node which ranked later than the
-            # current node
-            ind_toplogical_arrow_head = self.rng.integers(pos + 1, self.dag.num_nodes)
-            ind_arbitrary_arrow_head = self.dag.list_ind_nodes_sorted[
-                ind_toplogical_arrow_head
-            ]
-            # pos + 1 ensures ind_arbitrary_confound !=
-            # ind_arbitrary_arrow_head
-            if (
-                self.dag.mat_adjacency[ind_arbitrary_arrow_head,
-                                       ind_arbitrary_confound]
-                == 0
-            ):
-                self.dag.mat_adjacency[
-                    ind_arbitrary_arrow_head, ind_arbitrary_confound
-                ] = self._obj_gen_weight.gen(1)
-                self.dag.check()  # check if still a DAG
-            else:
-                # in very rare cases, the randomly chosen node is already a
-                # child of this current node
-                if force:
-                    # increase the toplogical order and try until success
-                    if ind_arbitrary_confound - 1 < 0:
-                        return False
-                    return self.mk_confound(
-                        ind_arbitrary_confound_input=ind_arbitrary_confound-1)
+            flag_success = self.add_new_edge(ind_arbitrary_confound)
+            # not successul in making this node to have two children
+        else:
+            return True  # already a confounder
+        if flag_success:
+            return True
+        # now flag_success must be False
+        if continue_top_up:
+            # increase the toplogical order and try until success
+            if ind_arbitrary_confound - 1 < 0:
                 return False
-                # not successul in making this node to have two children
-        return True
+            return self.mk_confound(
+                ind_arbitrary_confound_input=ind_arbitrary_confound - 1)
+        return False
+
+    def add_new_edge(self, ind_arbitrary_confound):
+        """
+        insert a new edge pointing to lower toplogical order
+        """
+        # get toplogical order of this node
+        pos = self.dag.list_ind_nodes_sorted.index(ind_arbitrary_confound)
+        # randomly choose one node which ranked later than the
+        # current node
+        if pos + 1 == self.dag.num_nodes:
+            return False
+        ind_toplogical_arrow_head = self.rng.integers(
+            pos + 1, self.dag.num_nodes)
+        ind_arbitrary_arrow_head = self.dag.list_ind_nodes_sorted[
+            ind_toplogical_arrow_head
+        ]
+
+        if (
+            self.dag.mat_adjacency[
+                ind_arbitrary_arrow_head, ind_arbitrary_confound]
+            == 0
+        ):
+            self.dag.mat_adjacency[
+                ind_arbitrary_arrow_head, ind_arbitrary_confound
+            ] = self._obj_gen_weight.gen(1)
+            self.dag.check()  # check if still a DAG
+            return True
+        # in very rare cases, the randomly chosen node is already a
+        # child of this current node
+        return False
