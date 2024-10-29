9c3773cdad15ad8b2c8ad8a9e04afe45105e2149
Alex Markham <alex.markham@causal.dev>
2024-10-29 13:44:02 +0100

    update names in test after refactor

--- tests/test_utils_closure.py
+++ tests/test_utils_closure.py
@@ -1,16 +1,11 @@
 import numpy as np
-from causalspyne.utils_closure import ancestor_matrix_transpose
+from causalspyne.utils_closure import ancestor_matrix
 
 
 def test_utils_transitive_closure():
     # Example usage note this adj matrix is opposite of our convention
-    adj_matrix = np.array([
-        [0, 1, 0, 0],
-        [0, 0, 1, 0],
-        [0, 0, 0, 1],
-        [0, 0, 0, 0]
-    ])
+    adj_matrix = np.array([[0, 1, 0, 0], [0, 0, 1, 0], [0, 0, 0, 1], [0, 0, 0, 0]])
 
-    ancestor_mat = ancestor_matrix_transpose(adj_matrix)
+    ancestor_mat = ancestor_matrix(adj_matrix)
     print("Ancestor matrix:")
     print(ancestor_mat.astype(int))
