8dcd796198caa52ec5c50451ec437050a78d4968
smilesun <smilesun.east@gmail.com>
2024-10-29 12:17:35 +0100

    doc for test 1

--- tests/test_dag2ancestral_adjmat.py
+++ tests/test_dag2ancestral_adjmat.py
@@ -11,46 +11,56 @@ def test_DAG2Ancestral_path():
     hide one node in a path
     """
     # lower triangular requires reverse top order
-    #     0, 1, 2, 3
-    #     A, B, C, D
+    #     0, 1, 2, 3 (column index)
+    #     A, B, C, D (as parent)
     adj_matrix = np.array(
-        [[0, 0, 0, 0], [1, 0, 0, 0], [0, 1, 0, 0], [0, 0, 1, 0]]  # A  # B  # C
-    )  # D
+        [[0, 0, 0, 0],     # row index 0, A as child
+         [1, 0, 0, 0],     # row index 1, B as child
+         [0, 1, 0, 0],     # row index 2, C as child
+         [0, 0, 1, 0]]     # row index 3, D as child
+    )
 
     # - entry(1,0): entry(B, A), A->B
-    # - entry(2,1): B->C
-    # - entry(3,2): A->B
+    # - entry(2,1): etnry(C, B), B->C
+    # - entry(3,2): C->D
     #
 
-    # if we hide 1
-    # 0->2 * new
-    # 2->3 intact
+    # if we hide B with index 1
+    # 0->2 or A->C  (* new)
+    # 2->3 or C->D  (edge left intact)
+
     # the full matrix
+    #      0, 1, 2, 3 (column index, parent)
+    #      A, B, C, D
+    #    [[0, 0, 0, 0],   # row index 0, A as child
+    #     [1, 0, 0, 0],   # row index 1, B as child
+    #     [1, 1, 0, 0],   # row index 2, C as child
+    #     [0, 0, 1, 0]])  # row index 3, D as child
+    # new edge is (2,0) or (C, A) correponding to A->C
 
-    #      3, 2, 1, 0
+    # delete index 1 row
+    #      0, 1, 2, 3 (column index, parent)
+    #      A, B, C, D
+    #    [[0, 0, 0, 0],   # row index 0, A as child
+    #     [1, 1, 0, 0],   # row index 2, C as child
+    #     [0, 0, 1, 0]])  # row index 3, D as child
 
-    #    [[0, 0, 0, 0],   # 3
-    #     [1, 0, 0, 0],   # 2
-    #     [0, 1, 0, 0],   # 1
-    #     [0, 1, 1, 0]])  # 0
-    # delete index 2 row
-    #    [[0, 0, 0, 0],   # 3
-    #     [1, 0, 0, 0],   # 2
-    #     [0, 1, 1, 0]])  # 0
+    # delete index 1 column
+    #      0, 2, 3
+    #      A, C, D (parent)
+    #    [[0, 0, 0],   # row index 0, A as child
+    #     [1, 0, 0],   # row index 2, C as child
+    #     [0, 1, 0]])  # row index 3, D as child
 
-    # delete index 2 column
-    #    [[0, 0, 0],   # 3
-    #     [1, 0, 0],   # 2
-    #     [0, 1, 0]])  # 0
+    sub_matrix = np.array(
+        [[0, 0, 0],
+         [1, 0, 0],
+         [0, 1, 0]])
 
-    # submatrix
-    #     3, 2, 0
-    sub_matrix = np.array([[0, 0, 0], [1, 0, 0], [0, 1, 0]])  # 3  # 2  # 0
-    # Y=Bu
     # correct ancestral graph
     ancestor_graph_matrix = sub_matrix
     obj = DAG2Ancestral(adj_matrix)
-    pred_ancestral_graph = obj.run([2])
+    pred_ancestral_graph = obj.run([1])
     assert (ancestor_graph_matrix == pred_ancestral_graph).all()
 
 
@@ -103,5 +113,5 @@ def test_DAG2Ancestral_complicated():
     )
 
     obj = DAG2Ancestral(adj_matrix)
-    pred_ancestral_graph = obj.run([5])  # hide H
+    pred_ancestral_graph = obj.run([2])  # hide H
     assert (ancestral_graph == pred_ancestral_graph).all()
