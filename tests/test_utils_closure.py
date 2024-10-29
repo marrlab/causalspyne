44a0ea57e465fb00543b2efb33dac39fc6f62928
smilesun <smilesun.east@gmail.com>
2024-10-29 14:37:28 +0100

    more unit test

--- tests/test_utils_closure.py
+++ tests/test_utils_closure.py
@@ -71,3 +71,36 @@ def test_utils_transitive_closure_transpose():
          [1, 1, 0, 0],     # C row as parent
          [1, 1, 1, 0]])    # D row as parent
     assert (ground_truth_ancestor_matrix == pred_ancestor_mat).all()
+
+
+def test_utils_transitive_closure_transpose2():
+    """
+    check if transitive closure correct
+    """
+    # Example usage note this adj matrix is opposite of our convention
+    # A->B, B->C, C->D, A->c
+    #     A, B, C, D (column index as parent)
+    adj_matrix = np.array(
+        [[0, 0, 0, 0],     # A row as child
+         [1, 0, 0, 0],     # B row as child
+         [1, 1, 0, 0],     # C row as child
+         [0, 0, 1, 0]])    # D row as child
+    pred_ancestor_mat = ancestor_matrix(adj_matrix)
+    print("pred Ancestor matrix:")
+    print(pred_ancestor_mat.astype(int))
+    # A->D(new)
+    # B->D(new)
+    #  A   B   C D
+    # [[
+    #  0   0   0 0]   # A row as child
+    # [1   0   0 0]   # B row as child
+    # [(1) 1   0 0]   # C row as child
+    # [(1) (1) 1 0]]  # D row as child
+
+    #     A, B, C, D
+    ground_truth_ancestor_matrix = np.array(
+        [[0, 0, 0, 0],     # A row as parent
+         [1, 0, 0, 0],     # B row as parent
+         [1, 1, 0, 0],     # C row as parent
+         [1, 1, 1, 0]])    # D row as parent
+    assert (ground_truth_ancestor_matrix == pred_ancestor_mat).all()
