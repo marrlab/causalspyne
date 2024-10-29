a77cbdd1274383def6a73755c3d15ec6d6adb20f
smilesun <smilesun.east@gmail.com>
2024-10-29 11:04:15 +0100

    refine

--- tests/test_dag2ancestral_adjmat.py
+++ tests/test_dag2ancestral_adjmat.py
@@ -10,7 +10,12 @@ def test_DAG2Ancestral_path():
     """
     hide one node in a path
     """
-    adj_matrix = np.array([[0, 0, 0, 0], [1, 0, 0, 0], [0, 1, 0, 0], [0, 0, 1, 0]])
+    adj_matrix = np.array(
+        [[0, 0, 0, 0],
+         [1, 0, 0, 0],
+         [0, 1, 0, 0],
+         [0, 0, 1, 0]])
+
     # - entry(1,0): 0->1
     # - entry(2,1): 1->2
     # - entry(3,2): 2->3
@@ -29,23 +34,15 @@ def test_DAG2Ancestral_path():
 
 def test_DAG2Ancestral_complicated():
     """
-     hide a node with parents and multiple children, resulting in both
-     kinds of directed edges as well as bidirected edges
+    hide a node with parents and multiple children, resulting in both
+    kinds of directed edges as well as bidirected edges
 
-     Original Graph:
+    Original Graph:
 
      A -> B -> H -> {C, E, F}
      C -> D -> E
      F -> G
 
-    Updated Graph after hiding H:
-
-     A -> B
-     B -> {C, E, F} *new
-     C -> D -> E
-     C -> E *new
-     F -> G
-     C <-> E <-> F *new
     """
     #        G, F, E, D, C, H, B, A
     adj_matrix = np.array(
@@ -53,7 +50,7 @@ def test_DAG2Ancestral_complicated():
             [0, 0, 0, 0, 0, 0, 0, 0],  # G
             [1, 0, 0, 0, 0, 0, 0, 0],  # F
             [0, 0, 0, 0, 0, 0, 0, 0],  # E
-            [0, 1, 0, 0, 0, 0, 0, 0],  # D
+            [0, 0, 1, 0, 0, 0, 0, 0],  # D
             [0, 0, 0, 1, 0, 0, 0, 0],  # C
             [0, 1, 1, 0, 1, 0, 0, 0],  # H
             [0, 0, 0, 0, 0, 1, 0, 0],  # B
@@ -61,6 +58,15 @@ def test_DAG2Ancestral_complicated():
         ]
     )
 
+    # Updated Graph after hiding H:
+    # A -> B
+    # B -> {C, E, F} *new
+    # C -> D -> E
+    # C -> E *new
+    # F -> G
+    # C <-> F <-> E *new
+
+    #        G, F, E, D, C, B, A
     ancestral_graph = np.array(
         [
             [0, 0, 0, 0, 0, 0, 0],  # G
@@ -74,5 +80,5 @@ def test_DAG2Ancestral_complicated():
     )
 
     obj = DAG2Ancestral(adj_matrix)
-    pred_ancestral_graph = obj.run([2])  # hide H
+    pred_ancestral_graph = obj.run([5])  # hide H
     assert (ancestral_graph == pred_ancestral_graph).all()
