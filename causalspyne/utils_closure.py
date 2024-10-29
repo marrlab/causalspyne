809ffbe0b0e72ed81d47071d19eaa8682e5ddf64
smilesun <smilesun.east@gmail.com>
2024-10-29 14:40:40 +0100

    .

--- causalspyne/utils_closure.py
+++ causalspyne/utils_closure.py
@@ -3,7 +3,9 @@ import numpy as np
 
 def transitive_closure(adj_matrix):
     """
-    Floyd-Warshall algorithm
+    Floyd-Warshall algorithm, note that this algorithm is agnostic w.r.t.
+    the edge convention: (i,j) implies either i<-j or i->j is the same
+    for the algorithm
     """
     num_vertex = len(adj_matrix)
     closure = np.array(adj_matrix, dtype=bool)
@@ -13,8 +15,11 @@ def transitive_closure(adj_matrix):
             for j in range(num_vertex):
                 # as long as one of the three cases result in connection, then
                 # i, j should be connected
-                closure[i][j] = closure[i][j] or (closure[i][k] and closure[k][j])
+                closure[i][j] = closure[i][j] or \
+                    (closure[i][k] and closure[k][j])
                 # i-k, k-j implies i-j
+                # here the mathematical relation i-j or i~j, can mean
+                # both i->j or j<-i
     return closure
 
 
