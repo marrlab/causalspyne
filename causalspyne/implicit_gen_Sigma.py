59284d819837c3558a23a1610b1cb02ec27656e9
smilesun <smilesun.east@gmail.com>
2025-02-16 11:53:38 +0100

    .

--- causalspyne/implicit_gen_Sigma.py
+++ causalspyne/implicit_gen_Sigma.py
@@ -1,11 +1,4 @@
 """
-This module is used to generate the implicit omega matrix.
-
-To generate a symmetric matrix,
-    - A+A^T
-    - np.tril(A) + np.tril(A, -1).T
-    np.tril(A, -1) returns a copy of the input array A with elements above the
-    first sub-diagonal zeroed out
 """
 import numpy as np
 from causalspyne.implicit_omega_condition_on_w import gen_joint_w_omega
--- causalspyne/implicit_omega_condition_on_w.py
+++ causalspyne/implicit_omega_condition_on_w.py
@@ -1,11 +1,4 @@
 """
-This module is used to generate the implicit omega matrix.
-
-To generate a symmetric matrix,
-    - A+A^T
-    - np.tril(A) + np.tril(A, -1).T
-    np.tril(A, -1) returns a copy of the input array A with elements above the
-    first sub-diagonal zeroed out
 """
 import numpy as np
 from numpy.random import default_rng
@@ -23,6 +16,7 @@ def gen_joint_w_omega(p_ob_v=4, ave_degree=2, max_omega=0.7):
     print(f"min, max eigenvalue {min_eig, max_eig}")
     print(f"rho, 2*rho: {rho}, {2 * rho}")
     return mat_w, mat_omega
+
 
 if __name__ == "__main__":
     gen_joint_w_omega()
