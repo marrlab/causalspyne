2838e425f1cd1717c05a61023669d860841364da
smilesun <smilesun.east@gmail.com>
2025-02-16 11:59:15 +0100

    .,

--- causalspyne/implicit_gen_Sigma.py
+++ causalspyne/implicit_gen_Sigma.py
@@ -6,7 +6,9 @@ from causalspyne.implicit_omega_condition_on_w import gen_joint_w_omega
 
 def gen_Sigma():
     mat_w, mat_omega = gen_joint_w_omega()
-    np.linalg.inv(np.identiy(mat_w.ndim)-mat_w)
+    left = np.linalg.inv(np.identiy(mat_w.ndim)-mat_w)
+    temp = np.matmul(left, mat_omega)
+    sigma = np.matmul(temp, np.identiy(mat_w.ndim)-mat_w))
 
 
 if __name__ == "__main__":
