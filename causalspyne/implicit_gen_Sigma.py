416e015820874c8aee8dfa8cafcaf4862b292104
smilesun <smilesun.east@gmail.com>
2025-02-17 11:56:40 +0100

    spec preciion

--- causalspyne/implicit_gen_Sigma.py
+++ causalspyne/implicit_gen_Sigma.py
@@ -4,6 +4,18 @@ import numpy as np
 from causalspyne.implicit_omega_condition_on_w import gen_joint_w_omega, get_max_degree
 from causalspyne.implicit_omega import get_extreme_eigenvalue
 
+def cov_to_corr(covariance_matrix):
+    # Calculate the standard deviations
+    std_devs = np.sqrt(np.diag(covariance_matrix))
+
+    # Create a diagonal matrix of inverse standard deviations
+    inv_std_devs = np.diag(1 / std_devs)
+
+    # Calculate the correlation matrix
+    correlation_matrix = inv_std_devs @ covariance_matrix @ inv_std_devs
+
+    return correlation_matrix
+
 
 def congruent(mat_op, mat):
     """
@@ -29,13 +41,19 @@ def gen_sigma_y(max_w=0.5):
     kernel = np.identity(mat_w.shape[0]) - mat_w
     inv_id_minus_w = np.linalg.inv(kernel)
     mat_sigma = congruent(inv_id_minus_w.T, mat_omega)
-    return mat_sigma, dmax
+    max_omega = np.max(mat_omega)
+    return mat_sigma, dmax, max_omega
 
 
 if __name__ == "__main__":
-    mat_sigma, dmax = gen_sigma_y()
+    mat_sigma, dmax, max_omega = gen_sigma_y()
     print(f"sigma: {mat_sigma}")
     inv_sigma = np.linalg.inv(mat_sigma)
     eigv_max, _ = get_extreme_eigenvalue(inv_sigma)
     print(f"max eigen value: {eigv_max}")
-    print(f"max degree: {dmax}")
+    print(f"max degree: {dmax}, max_omega: {max_omega}")
+
+    mat_precision = cov_to_corr(inv_sigma)
+    eigv_max, _ = get_extreme_eigenvalue(mat_precision)
+    print(f"max eigen value: {eigv_max}")
+
