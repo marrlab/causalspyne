69685c6e41bc4297aa8ad14ec353688c7ba2ddcd
smilesun <smilesun.east@gmail.com>
2025-02-17 11:43:51 +0100

    degree

--- causalspyne/implicit_gen_Sigma.py
+++ causalspyne/implicit_gen_Sigma.py
@@ -1,7 +1,7 @@
 """
 """
 import numpy as np
-from causalspyne.implicit_omega_condition_on_w import gen_joint_w_omega
+from causalspyne.implicit_omega_condition_on_w import gen_joint_w_omega, get_max_degree
 from causalspyne.implicit_omega import get_extreme_eigenvalue
 
 
@@ -19,15 +19,17 @@ def gen_sigma_y():
     generate Sigma matrix according to $${(I-W)}^{-1}\Omega{(I-W)}^{-1}$$
     """
     mat_w, mat_omega = gen_joint_w_omega()
+    dmax = get_max_degree(mat_w)
     kernel = np.identity(mat_w.shape[0]) - mat_w
     inv_id_minus_w = np.linalg.inv(kernel)
     mat_sigma = congruent(inv_id_minus_w.T, mat_omega)
-    return mat_sigma
+    return mat_sigma, dmax
 
 
 if __name__ == "__main__":
-    mat_sigma = gen_sigma_y()
+    mat_sigma, dmax = gen_sigma_y()
     print(f"sigma: {mat_sigma}")
     inv_sigma = np.linalg.inv(mat_sigma)
     eigv_max, _ = get_extreme_eigenvalue(inv_sigma)
     print(f"max eigen value: {eigv_max}")
+    print(f"max degree: {dmax}")
--- causalspyne/implicit_omega_condition_on_w.py
+++ causalspyne/implicit_omega_condition_on_w.py
@@ -6,6 +6,17 @@ from causalspyne.erdo_renyi_plp import Erdos_Renyi_PLP
 from causalspyne.implicit_omega import gen_omega, get_extreme_eigenvalue
 
 
+def get_max_degree(mat_w):
+    d_max = 0
+    for ind_row, row in enumerate(mat_w):
+        # w_{i,j} j->i
+        in_degree = len(np.nonzero(row))
+        out_degree = len(np.nonzero(mat_w[:, ind_row]))
+        if in_degree + out_degree > d_max:
+            d_max = in_degree + out_degree
+    return d_max
+
+
 def gen_joint_w_omega(p_ob_v=4, ave_degree=2, max_omega=0.7):
     mat_w = Erdos_Renyi_PLP(default_rng(0))(num_nodes=p_ob_v,
                                             degree=ave_degree)
