703be7d0f31f70fd3cbe9321a96bae0f7144594e
smilesun <smilesun.east@gmail.com>
2025-10-14 13:52:08 +0200

    ifelse edge model

--- causalspyne/edge_models.py
+++ causalspyne/edge_models.py
@@ -13,3 +13,20 @@ class EdgeModelLinear():
         weights = self._dag.get_weights_from_list_parents(ind_node)
         bias = np.dot(data_realization_parents, weights)
         return bias
+
+
+class EdgeModelIfElse():
+    def __init__(self, dag):
+        self._dag = dag
+
+    def run(self, ind_node, data_realization_parents):
+        """
+        data_realization_parents = data[:, list_parents_inds]
+        """
+        # Linear combination of parent nodes + Gaussian noise
+        weights = self._dag.get_weights_from_list_parents(ind_node)
+        weights_mod = np.where(data_realization_parents == 0,
+                               -weights, weights)
+        bias = np.dot(data_realization_parents, weightsdata_realization_parents_mod
+                      )
+        return bias
