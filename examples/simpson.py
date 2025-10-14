bb3801c770d40ed16e0a348a068552b3f3dc8fab
smilesun <smilesun.east@gmail.com>
2025-10-14 18:51:50 +0200

    invert color and marker

--- examples/simpson.py
+++ examples/simpson.py
@@ -7,16 +7,16 @@ arr = simpson(200)
 
 x = arr[:, 1]  # 2nd column
 y = arr[:, 2]  # 3rd column
-color = arr[:, 0]  # 1st column for color
+scenario = arr[:, 0]  # 1st column for scenario
 
-median0 = np.median(x[color == 0])
-median1 = np.median(x[color == 1])
+median0 = np.median(x[scenario == 0])
+median1 = np.median(x[scenario == 1])
 
-# Create indicator column: 1 if x > median for that z group, else 0
-indicator = np.zeros_like(x, dtype=int)
-indicator[(color == 0) & (x > median0)] = 1
-indicator[(color == 1) & (x > median1)] = 1
-arr_discrete_aug = np.column_stack((arr, indicator))
+# Create treatment column: 1 if x > median for that z group, else 0
+treatment = np.zeros_like(x, dtype=int)
+treatment[(scenario == 0) & (x > median0)] = 1
+treatment[(scenario == 1) & (x > median1)] = 1
+arr_discrete_aug = np.column_stack((arr, treatment))
 
 colors = ['green', 'orange']
 cmap = mcolors.ListedColormap(colors)
@@ -25,19 +25,18 @@ bounds = [-0.5, 0.5, 1.5]
 norm = mcolors.BoundaryNorm(bounds, cmap.N)
 marker_map = {0: 'o', 1: 's'}
 
-for ind_val in np.unique(indicator):
-    idx = np.where(indicator == ind_val)
-    scatter = plt.scatter(x[idx], y[idx], c=color[idx],
+for ind_val in np.unique(scenario):
+    idx = np.where(scenario == ind_val)
+    scatter = plt.scatter(x[idx], y[idx], c=treatment[idx],
                           marker=marker_map[ind_val],
                           edgecolor='k',
-                          label=f'algo {ind_val}',
+                          label=f'scenario {ind_val}',
                           cmap=cmap, norm=norm, s=100)
 
 plt.xlabel('treatment')
 plt.ylabel('performance')
-# plt.colorbar(label='scenario')
 cbar = plt.colorbar(scatter, boundaries=bounds, ticks=[0, 1])
-cbar.ax.set_yticklabels(['Scenario 1', 'Scenario 2'])
-plt.legend(title='algorithm')
+cbar.ax.set_yticklabels(['algorithm 1', 'algorithm 2'])
+plt.legend(title='scenario')
 plt.title('treatment effect')
 plt.show()
