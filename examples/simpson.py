fc923cece3418063274f4e455096da3f8aba0de3
smilesun <smilesun.east@gmail.com>
2025-10-14 18:26:30 +0200

    continuous simpson

--- examples/simpson.py
+++ examples/simpson.py
@@ -1,5 +1,6 @@
 from causalspyne.dataset import simpson
 import matplotlib.pyplot as plt
+import matplotlib.colors as mcolors
 
 arr = simpson(200)
 
@@ -7,9 +8,18 @@ x = arr[:, 1]  # 2nd column
 y = arr[:, 2]  # 3rd column
 color = arr[:, 0]  # 1st column for color
 
-plt.scatter(x, y, c=color, cmap='viridis')
-plt.xlabel('2nd Column')
-plt.ylabel('3rd Column')
-plt.colorbar(label='1st Column')
-plt.title('Scatter plot of 2nd vs 3rd column colored by 1st column')
+colors = ['green', 'orange']
+cmap = mcolors.ListedColormap(colors)
+# Boundaries separate the two values: 0 and 1
+bounds = [-0.5, 0.5, 1.5]
+norm = mcolors.BoundaryNorm(bounds, cmap.N)
+scatter = plt.scatter(x, y, c=color, cmap=cmap, norm=norm, s=100)
+
+plt.xlabel('treatment')
+plt.ylabel('performance')
+# plt.colorbar(label='scenario')
+cbar = plt.colorbar(scatter, boundaries=bounds, ticks=[0, 1])
+cbar.ax.set_yticklabels(['Scenario 1', 'Scenario 2'])
+
+plt.title('treatment effect')
 plt.show()
