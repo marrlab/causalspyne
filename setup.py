f818d91ed0a05e88a91edafc62e2bb5ae0cc3281
smilesun <smilesun.east@gmail.com>
2025-10-16 11:31:08 +0200

    update 0.1.6

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="causalspyne",
-    version="0.1.5",
+    version="0.1.6",
     packages=find_packages(),  # Ensure your package folder is inside the find_packages scope
 )
 
